893f24d5d9b2a481dfbfbc9188945b5321c04308
shivram <shivram.visiontemenos.com>
2019-08-06 11:14:06 -0400

    changes made by Soma but not updated

--- WikiTesting/src/scripts/GoogleTest.java
+++ WikiTesting/src/scripts/GoogleTest.java
@@ -1,9 +1,19 @@
 package scripts;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class GoogleTest {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) 
+	
+	{
 		// TODO Auto-generated method stub
+		
+		System.setProperty("webdriver.chrome.driver", "src/resources/drivers/chromedriver");
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.co.in");
 
 	}
 
