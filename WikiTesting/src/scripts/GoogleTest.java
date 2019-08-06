0226e1478c1b58231fa3779c9d081e3dbae54f68
shivram <shivram.visiontemenos.com>
2019-08-06 11:11:46 -0400

    updated enhance from Kum

--- WikiTesting/src/scripts/GoogleTest.java
+++ WikiTesting/src/scripts/GoogleTest.java
@@ -1,9 +1,17 @@
 package scripts;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class GoogleTest {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	
+	{
+		System.setProperty("webdriver.chrome.driver", "src/resources/drivers/chromedriver");
+
+		WebDriver driver = new ChromeDriver();
+		driver.get("https://www.google.com");
 
 	}
 
